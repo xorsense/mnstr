5109603d554b386a9aa365f06ce2f93c2ba64b29
magickibbles <abi.chr567@gmail.com>
2023-10-13 18:00:39 -0400

    assets getter for mnstr parts

--- lib/widgets/monster.dart
+++ lib/widgets/monster.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+
+import 'monster_parts.dart';
 import '../generator/monster.dart';
 
 class MonsterWidget extends StatelessWidget {
@@ -16,21 +18,22 @@ class MonsterWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(
-              child: Column(
-                children: [
-                  Text('head: ${monster.head}'),
-                  Text('horns: ${monster.horns}'),
-                  Text('arms: ${monster.arms}'),
-                  Text('legs: ${monster.legs}'),
-                  Text('tail: ${monster.tail}'),
-                ],
-              ),
+              child:
+                  Column(children: MonsterParts(monster: monster).monsterParts
+                      // [
+                      // Text('head: ${monster.head}'),
+                      // Text('horns: ${monster.horns}'),
+                      // Text('arms: ${monster.arms}'),
+                      // Text('legs: ${monster.legs}'),
+                      // Text('tail: ${monster.tail}'),
+                      // ],
+                      ),
             ),
           ),
           SizedBox(
-                  child: Container(
-                    color: monster.color,
-                  )),
+              child: Container(
+            color: monster.color,
+          )),
         ]),
       ),
     );
